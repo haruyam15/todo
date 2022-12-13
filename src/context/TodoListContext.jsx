8c01dc1a2bcabae8a0062422d49cd2991f2b587e
Haeun <haruyam15@gmail.com>
2022-12-13 17:59:58 +0900

    완성

--- src/context/TodoListContext.jsx
+++ src/context/TodoListContext.jsx
@@ -8,7 +8,10 @@ const TodoListContext = createContext();
 export default function TodoListProvider({children}){
 
     const [todoList, dispatch] = useReducer(todoListReducer, getTodoList());
-    const [tabs, setTabs] = useState(getTabs());
+    const [tabs, setTabs] = useState(getTabs); 
+    //getTabs()로 전달하게되면 상태가 업데이트 될때마다 계속 호출함
+    //useState(()=>getTabs())처럼 한번 감싸서 전달하면 한번만 호출
+    //인자와 호출하는 내용이 동일하다면 함수의 이름(참조값)만 전달해도 되므로 getTabs로 전달
     const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')) ? JSON.parse(localStorage.getItem('darkMode')) : false);
 
     const darkModeToggle = () => {
@@ -67,32 +70,38 @@ export default function TodoListProvider({children}){
     };
 
     function getTodoList(){
+        console.log('getTodoList');
         const todoList = JSON.parse(localStorage.getItem('todoList'))
         if(todoList){
             return todoList;
+        }else{
+            return [];
         }
-        return [];
+        
     };
 
     function getTabs(){
+        console.log('getTabs');
         const tabs = JSON.parse(localStorage.getItem('tabs'));
         if(tabs){
             return tabs;
-        };
-        return [
-            {
-                name : 'All',
-                selected :true
-            },
-            {
-                name : 'Active',
-                selected :false
-            },
-            {
-                name : 'Completed',
-                selected :false
-            },
-        ];
+        }else{
+            return [
+                {
+                    name : 'All',
+                    selected :true
+                },
+                {
+                    name : 'Active',
+                    selected :false
+                },
+                {
+                    name : 'Completed',
+                    selected :false
+                },
+            ];
+        }
+        
     };
      
     return(
